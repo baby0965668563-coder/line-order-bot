cb1bf0b8060a3a0bc5dc1b265e76f540ac6bf30e
baby0965668563-coder <baby0965668563@gmail.com>
2026-04-25 13:00:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ function handleEvent(event) {
   });
 }
 
-app.listen(3000, () => {
-  console.log('Server running on 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
