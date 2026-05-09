ea405f24cfac2e6cd32e87ebced52f1c9cead267
baby0965668563-coder <baby0965668563@gmail.com>
2026-05-10 01:10:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -346,16 +346,7 @@ button{
   id="optionModal"
 >
 
-<div class=".modal-box{
-  background:white;
-  width:90%;
-  max-width:420px;
-  max-height:80vh;
-  overflow-y:auto;
-  margin:50px auto;
-  border-radius:20px;
-  padding:20px;
-}">
+<div class="modal-box">
 
 <h2 id="modalTitle"></h2>
 
